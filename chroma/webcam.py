bf2626869bcf0c3c52152376971a92933534c462
javierre <javierperezlara@gmail.com>
2021-01-01 15:20:37 +0100

    Create webcam.py

---
+++ chroma/webcam.py
@@ -0,0 +1,23 @@
+import cv2
+
+cv2.namedWindow("preview")
+
+#internal camera
+cap = cv2.VideoCapture(0)
+#external
+#cap = cv2.VideoCapture(1)
+
+if cap.isOpened(): # try to get the first frame
+    ret, frame = cap.read()
+else:
+    ret = False
+
+cv2.namedWindow("chroma")
+
+while(ret):
+    ret, frame = cap.read()
+    key = cv2.waitKey(20)
+    cv2.imshow('chroma', frame)
+    if key == 27: # exit on ESC
+        break    
+cv2.destroyWindow("preview")
